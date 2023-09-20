7b40c1cff3ff9317422dc16ae4af4d3fb1d0e3a2
Yazan Kiswani <yznkiswani@gmail.com>
2023-09-20 06:19:52 +0300

    PHPtoHTML

--- header.php
+++ header.php
@@ -20,54 +20,7 @@
     <title>HTUMUN</title>
   </head>
   <body>
-    <header role="banner">
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <div class="container">
-          <!-- <a class="navbar-brand" href="index.html"><img style="" src="HTUMUNLogo.svg" alt=""></a> -->
-          <div class="navbar-brand"><a style="display: inline;" href="/index.html"><img src="HTUMUNLogo.svg" alt=""></a></div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarsExample05">
-            <ul class="navbar-nav ml-auto pl-0">
-              <li class="nav-item">
-                <a class="nav-link" href="comingsoon.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="comingsoon.html">Legacy</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Committees</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown04">
-                  <a class="dropdown-item" href="comingsoon.html">Committee 1</a>
-                  <a class="dropdown-item" href="comingsoon.html">Committee 2</a>
-                  <a class="dropdown-item" href="comingsoon.html">Committee 3</a>
-                  <a class="dropdown-item" href="comingsoon.html">Committee 4</a>
-                  <a class="dropdown-item" href="comingsoon.html">Committee 5</a>
-                  <a class="dropdown-item" href="comingsoon.html">Committee 6</a>
-                  <a class="dropdown-item" href="comingsoon.html">Committee 7</a>
-                  <a class="dropdown-item" href="comingsoon.html">Committee 8</a>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="comingsoon.html">Media</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="comingsoon.html">Contact Us</a>
-              </li>
-            </ul>
-
-            <ul class="navbar-nav mx-auto">
-              <li class="nav-item register-btn">
-                <a class="" href="comingsoon.html">Register Now!</a>
-              </li>
-            </ul>
-            
-          </div>
-        </div>
-      </nav>
-    </header>
+    
     <!-- END header -->
     <!-- <div class="hero" style="background-color: black;"></div> -->
   </body>
